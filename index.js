ce4eae9b96f635850ec0cbc20edee8112426c9ee
sahilm3hra <rap.star5567@gmail.com>
2022-12-18 19:26:58 +0530

    project setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+
+app.listen(port, (err) => { 
+    if (err) {
+        console.log(`Error in running the server : ${err}`);
+    }
+    console.log(`Server is running on port : ${port}`);
+})
