b66ed81f2ebba1c91b414f87205f60986920c360
sahilm3hra <rap.star5567@gmail.com>
2022-12-19 22:10:19 +0530

    set up route and views for signIn and signUp

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,17 @@ module.exports.profile = function (req, res) {
         title : 'Users'
     })
 }
+
+// rendering signIn view
+module.exports.signIn = function(req, res) {
+    return res.render('user_sign_in', {
+        title : 'Sign In'
+    })
+}
+
+// rendering signUp view
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title : 'Sign Up'
+    })
+}
