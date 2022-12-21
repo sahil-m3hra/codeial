e72dc0d39a908710b2ca0caeaae6eb79d4e527dd
sahilm3hra <rap.star5567@gmail.com>
2022-12-21 22:03:07 +0530

    set up manula authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,22 @@
 const User = require('../models/user');
 
 module.exports.profile = function (req, res) {
-    return res.render('users', {
-        title : 'Users'
-    })
+    if (req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if(user){
+                return res.render('users', {
+                    title: "User Profile",
+                    user: user
+                })
+            }else {
+                return res.redirect('users/sign-in')
+            }
+            
+        });
+    }else{
+        return res.render('/users/sign-in');
+    }
+  
 }
 
 // rendering signIn view
@@ -49,5 +62,33 @@ module.exports.create = function(req, res){
 
 // SignIn and create a session for the user
 module.exports.createSession = function(req, res){
-    // todo later
+    // steps tp authenticate
+    // find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('error in finding the user in signing in');
+            return
+        }
+        
+        if (user){
+            // handle passowrd which does'nt match
+            if (user.password != req.body.password){
+                return res.redirect('back');
+            }
+            // handle user found
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+
+        }else{
+            // handle user not found
+            return res.redirect('back')
+        }
+    })
+}
+
+// deleting session
+
+module.exports.clearSession = function(req, res){
+    req.clearCookie('user_id');
+    return res.redirect('/users/sign-in');
 }
--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,8 @@ router.get('/sign-up', userController.signUp);
 
 router.post('/create', userController.create);
 
+router.post('/create-session', userController.createSession);
+
+router.get('/clear-session', userController.clearSession);
+
 module.exports = router;
